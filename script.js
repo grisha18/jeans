2efd04b2ffbd497cd11aaadb3f9863c4cd55fed2
grisha7777 <kurdjievgrigory777@gmail.com>
2020-11-20 15:42:51 +0300

    range-right

--- script.js
+++ script.js
@@ -19,8 +19,6 @@ const mousemoveHandler = (e) =>{
         rangeLeft.style.left = `${offSet}px`;
     }
 }
-
-
 const clickHandler = (e) =>{
     rangeLeft.addEventListener('mousemove', mousemoveHandler);
     rangeLeft.addEventListener('mouseup', 
@@ -31,3 +29,19 @@ rangeLeft.addEventListener('mousedown', clickHandler )
 // MOUSELEAVE
 
 // и черную линию обработать
+
+const mousemoveClick = (e) => {
+    if( (e.x < rangeRightX) && (e.x < currentLeft) ){
+        console.log('hihi');
+        let w = getComputedStyle(rangeRight).getPropertyValue('--circle-width');
+        w = parseInt(w);
+        let offSet = e.x - rangeRightX - w/2;
+        rangeRight.style.right = `${offSet}px`; 
+    }
+}
+const click = (e) =>{
+    rangeRight.addEventListener('mousemove', mousemoveClick);
+    rangeRight.addEventListener('mouseup', 
+            () => rangeRight.removeEventListener('mousemove', mousemoveClick));
+}
+rangeRight.addEventListener('mousedown', click)
