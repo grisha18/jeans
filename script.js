c592d266a68180d7b0199fca0afe1da82fcef9c6
maks <a@a.a>
2020-11-21 11:03:42 +0300

    diapazon

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
 const range__slider = document.querySelector('.range__slider');
 const rangeLeft = document.querySelector('.range__left');
 const rangeRight = document.querySelector('.range__right');
+const shopLineBlack = document.querySelector('.shop-line__black');
+const priceSliderNumber =  document.querySelector('.price-slider__number');
+const priceRangeNumber = document.querySelector('.price-range__number');
 
 
-const rangeLeftX = rangeLeft.getBoundingClientRect().x
-const rangeRightX = rangeRight.getBoundingClientRect().x
+
+const rangeLeftX = rangeLeft.getBoundingClientRect().x;
+const rangeRightX = rangeRight.getBoundingClientRect().x;
+
+const length = rangeRightX - rangeLeftX;
+
+const minLeft = 0;
+const maxRight = 1000;
+const diapazon = maxRight - minLeft;
+priceSliderNumber.innerHTML = `$ ${minLeft}`;
+priceRangeNumber.innerHTML = `$ ${maxRight}`;
+
+
 let currentLeft = rangeLeftX;
 let currentRight = rangeRightX;
 
@@ -16,13 +30,24 @@ const mousemoveHandler = (e) =>{
         let w = getComputedStyle(rangeLeft).getPropertyValue('--circle-width');
         w = parseInt(w);
         let offSet = e.x - rangeLeftX - w/2;
+        currentLeft = rangeLeftX + offSet;
+        
+        
         rangeLeft.style.left = `${offSet}px`;
+        shopLineBlack.style.left = `${offSet}px`
+        const movedInPixel = (currentLeft - rangeLeftX) > 0 ? (currentLeft - rangeLeftX) : 0;
+        
+        const movedInPercent = movedInPixel * 100 / length;
+        
+        const Indollars = Math.round(( diapazon  / 100 ) * movedInPercent);
+        priceSliderNumber.innerHTML = `$ ${minLeft+Indollars}`;
+        
     }
 }
 const clickHandler = (e) =>{
-    rangeLeft.addEventListener('mousemove', mousemoveHandler);
-    rangeLeft.addEventListener('mouseup', 
-            () => rangeLeft.removeEventListener('mousemove', mousemoveHandler));
+    document.addEventListener('mousemove', mousemoveHandler);
+    document.addEventListener('mouseup', 
+            () => document.removeEventListener('mousemove', mousemoveHandler));
 }
 rangeLeft.addEventListener('mousedown', clickHandler )
 
@@ -33,16 +58,24 @@ rangeLeft.addEventListener('mousedown', clickHandler )
 const mousemoveClick = (e) => {
     let w = getComputedStyle(rangeRight).getPropertyValue('--circle-width');
     w = parseInt(w);
-
-    if( (e.x < (rangeRightX + w/2)) && (e.x > (currentLeft + w)) ){
-        console.log('hihi');
+    console.log(rangeRightX, e.x)   
+    if( (e.x < (rangeRightX + w + 1)) && (e.x > (currentLeft + w)) ){
+        console.log(rangeRightX, e.x)
         let offSet =  rangeRightX -e.x + w/2;
-        rangeRight.style.right = `${offSet}px`; 
+        rangeRight.style.right = `${offSet}px`;
+        shopLineBlack.style.right = `${offSet}px`;
+        currentRight = rangeRightX - offSet - w/2;
+        const movedInPixel = (rangeRightX - currentRight) > 0 ? (rangeRightX - currentRight) : 0;
+        
+       const movedInPercent = movedInPixel * 100 / length;
+    
+        const Indollars = Math.round(( diapazon  / 100 ) * movedInPercent);
+        priceRangeNumber.innerHTML = `$ ${maxRight-Indollars}`; 
     }
 }
 const click = (e) =>{
-    rangeRight.addEventListener('mousemove', mousemoveClick);
-    rangeRight.addEventListener('mouseup', 
-            () => rangeRight.removeEventListener('mousemove', mousemoveClick));
+    document.addEventListener('mousemove', mousemoveClick);
+    document.addEventListener('mouseup', 
+            () => document.removeEventListener('mousemove', mousemoveClick));
 }
 rangeRight.addEventListener('mousedown', click)
